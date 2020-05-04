9a3926fd90349b4a92716b6b6d7648acf0a335b0
Jonathan Ming <jming422@gmail.com>
2020-05-03 18:08:16 -0700

    Add licensing and update package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+/*
+ * Fridginator - Keep track of what's in your fridge (and how long it's been there)
+ * Copyright © 2020 Jonathan Ming
+ *
+ * Fridginator is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fridginator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fridginator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';

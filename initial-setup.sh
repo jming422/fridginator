c876fa164943435ba0739ebc7f2682f2a55e1f51
Jonathan Ming <jming422@gmail.com>
2020-05-09 00:56:40 -0700

    Fix dev db config and update script with messages

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+set -e
 
+echo '🗄 Creating local PostgreSQL database "fridginator"...'
 createdb fridginator
+
+echo '📁 Generating .env configuration file...'
+echo 'NODE_ENV=development' > .env
 echo 'DATABASE_URL=postgres://localhost/fridginator' >> .env
-echo 'NODE_ENV=development' >> .env
+
+echo '⚙️ Installing dependencies...'
 npm install
+
+echo '🚢 Executing database migrations...'
 npm run migrate up
+
+echo '🎉 Done! Run `npm start` to launch the app.'
